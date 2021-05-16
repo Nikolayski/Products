f49b5473d42c799db8a71d54c7549ba45f6c4464
Nikolayski <74212719+Nikolayski@users.noreply.github.com>
2021-05-16 17:01:03 +0300

    Modify home

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,10 +1,21 @@
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
 import React from 'react';
 import './Home.css';
 
 
 const Home = (props) => {
+
+
+  
     return (
-    <h1>Hello from home</h1>
+        <form>
+            <p>Get weather info</p>
+            <FormControl className="form-control">
+                <InputLabel htmlFor="town">Town</InputLabel>
+                <Input type="text" id="town" name="town" />
+            </FormControl>
+            <Button type="submit" variant="contained" color="primary">Search</Button>
+        </form>
     )
 }
 
